de05fea34044eeddb90f200894cf2a283e280fd1
Yuya Honda <64887971+yuyahnd@users.noreply.github.com>
2023-07-23 17:34:05 +0900

    Add project framework7

---
+++ src/pages/home.vue
@@ -0,0 +1,56 @@
+<template>
+  <f7-page name="home">
+    <!-- Top Navbar -->
+    <f7-navbar large :sliding="false">
+      <f7-nav-left>
+        <f7-link icon-ios="f7:menu" icon-md="material:menu" panel-open="left"></f7-link>
+      </f7-nav-left>
+      <f7-nav-title sliding>Mobile-Vue</f7-nav-title>
+      <f7-nav-right>
+        <f7-link icon-ios="f7:menu" icon-md="material:menu" panel-open="right"></f7-link>
+      </f7-nav-right>
+      <f7-nav-title-large>Mobile-Vue</f7-nav-title-large>
+    </f7-navbar>
+    <!-- Toolbar-->
+    <f7-toolbar bottom>
+      <f7-link>Left Link</f7-link>
+      <f7-link>Right Link</f7-link>
+    </f7-toolbar>
+    <!-- Page content-->
+    <f7-block>
+      <p>Here is your blank Framework7 app. Let's see what we have here.</p>
+    </f7-block>
+    <f7-block-title>Navigation</f7-block-title>
+    <f7-list strong inset dividersIos>
+      <f7-list-item link="/about/" title="About"></f7-list-item>
+      <f7-list-item link="/form/" title="Form"></f7-list-item>
+    </f7-list>
+
+    <f7-block-title>Modals</f7-block-title>
+    <f7-block class="grid grid-cols-2 grid-gap">
+      <f7-button fill popup-open="#my-popup">Popup</f7-button>
+      <f7-button fill login-screen-open="#my-login-screen">Login Screen</f7-button>
+    </f7-block>
+
+    <f7-block-title>Panels</f7-block-title>
+    <f7-block class="grid grid-cols-2 grid-gap">
+      <f7-button fill panel-open="left">Left Panel</f7-button>
+      <f7-button fill panel-open="right">Right Panel</f7-button>
+    </f7-block>
+
+    <f7-list strong inset dividersIos>
+      <f7-list-item
+        title="Dynamic (Component) Route"
+        link="/dynamic-route/blog/45/post/125/?foo=bar#about"
+      ></f7-list-item>
+      <f7-list-item
+        title="Default Route (404)"
+        link="/load-something-that-doesnt-exist/"
+      ></f7-list-item>
+      <f7-list-item
+        title="Request Data & Load"
+        link="/request-and-load/user/123456/"
+      ></f7-list-item>
+    </f7-list>
+  </f7-page>
+</template>
